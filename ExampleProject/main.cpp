cac3ebdec22ac2505a4c6771458e8e26b01ebbc6
Лебедев В. <vladlebedev09042007@gmail.com>
2026-09-04 15:44:01 +0300

    исправлен интерфейс пользователя

--- ExampleProject/main.cpp
+++ ExampleProject/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 int main() {
-	std::cout << "What^s up, man?" << std::endl;
-
-	std::cout << "Hello from dev branch!\n";
+	double a, h, s;
+	std::cout << "Enter side of a triangle" << std::endl;
+		while (!(std::cin >> a) || a <= 0) {
+			std::cin.clear();
+			std::cin.ignore(10, '\n');
+			std::cout << "Enter side of a triangle" << std::endl;
+		}
+	
+	std::cout << "Enter height of a triangle" << std::endl;
+		while (!(std::cin >> h) || h <= 0) {
+			std::cin.clear();
+			std::cin.ignore(10, '\n');
+			std::cout << "Enter height of a triangle" << std::endl;
+		}
+	
+	s = a * h / 2;
+	std::cout << "area of triangle:" << s;
 }
